29743e1fcc8e3ac3e52773468fc8656425d359e9
rasikaG23 <rasikargujar23@gmail.com>
2023-03-15 19:14:58 +0530

    Spaces-branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Spaces_Page from './components/Spaces-Page';
 
 function App() {
-  return <h2>Hello</h2>;
+  return <div>
+    <h2>Hello</h2>
+    <Spaces_Page/>
+    </div>
+    
 }
 
 export default App;
