e8c70beeb5c2160b63aa3ec8c639538241c4863f
arsh459 <arshgoyal459@gmail.com>
2023-03-15 18:29:16 +0530

    Login Page functionality  Completed

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,10 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
 import LoginComp from '../components/Login/LoginComp';
-
+import PrivateRoute from './Private';
+import { Home } from './home';
+import LoginRoute from './LoginRoute';
 function AllRoutes() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginComp />} />
+      <Route
+        path="/login"
+        element={
+          <LoginRoute>
+            <LoginComp />
+          </LoginRoute>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
