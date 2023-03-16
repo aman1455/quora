2f15dad7a6dc2b49112afcb62af7dbf9040d9e20
narayan dutta <narayandutta2208@gmail.com>
2023-03-17 03:45:21 +0530

    Music sec implement ui

---
+++ src/components/NavbarSidebar/Music/WholeMusicComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TabsMusic from './TabsMusic'
+function WholeMusicComp() {
+  return (
+    <>
+        <TabsMusic/>
+    </>
+  )
+}
+
+export default WholeMusicComp
