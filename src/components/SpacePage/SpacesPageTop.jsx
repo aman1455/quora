ff1b821802963e4b03cc48ea1cdc6d47346dd917
arsh459 <arshgoyal459@gmail.com>
2023-03-17 23:21:15 +0530

    Space Page Update

---
+++ src/components/SpacePage/SpacesPageTop.jsx
@@ -0,0 +1,82 @@
+import React from "react"
+import {
+  Box,
+  Flex,
+  VStack,
+  Image,
+  Icon,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react"
+import ReadMoreReact from "read-more-react"
+
+function SpacesPageTop() {
+  return (
+    <></>
+    // <Box
+    //   w="100%"
+    //   h="35vh"
+    //   display="flex"
+    //   direction="column"
+    //   alignItems="flex-end"
+    //   justifyContent={"center"}
+    //   border="1px solid red"
+    // >
+    //   <Box w="75%" border="1px solid red" filter="blur">
+    //     <Box mb={2}>
+    //       <Heading fontSize="27px">HTML CSS and JavaScript</Heading>
+    //     </Box>
+    //     <Box mb={2}>
+    //       <Text fontSize="13px">
+    //         Here we learn HTML, CSS and JavaScript with cool projects✨
+    //       </Text>
+    //     </Box>
+    //     <Box
+    //       mb={2}
+    //       display="flex"
+    //       justifyContent="space-between"
+    //       align="center"
+    //     >
+    //       <Box fontSize="13px">
+    //         43 Contributors 5.6K followers22 posts in the last week
+    //       </Box>
+    //       <Box display="flex" align="center">
+    //         <Box mr={3}>
+    //           <Icon viewBox="0 0 24 24">
+    //             <path
+    //               stroke-width="1.5"
+    //               stroke="#666"
+    //               fill="none"
+    //               d="M5 14a2 2 0 1 1 0-4 2 2 0 0 1 0 4Zm7 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4Zm7 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4Z"
+    //             />
+    //           </Icon>
+    //         </Box>
+    //         <Box>
+    //           <Button
+    //             leftIcon={
+    //               <Icon viewBox="0 0 24 24">
+    //                 <path
+    //                   stroke="#666"
+    //                   stroke-width="1.5"
+    //                   fill="none"
+    //                   stroke-linecap="round"
+    //                   stroke-linejoin="round"
+    //                   d="M13.5 19.5h-6a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v6M8 12.5v-4h4v4H8zM14.5 9H16h-1.5zm0 3H16h-1.5zM8 15.5h8-8zm9 4h5M19.5 17v5"
+    //                 ></path>
+    //               </Icon>
+    //             }
+    //             colorScheme="teal"
+    //             variant="outline"
+    //           >
+    //             Follow Space
+    //           </Button>
+    //         </Box>
+    //       </Box>
+    //     </Box>
+    //   </Box>
+    // </Box>
+  )
+}
+
+export default SpacesPageTop
